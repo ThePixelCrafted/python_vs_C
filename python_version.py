839b6b66211dc09c49df82c225c9a6145f7f5757
ThePixelCrafted <raphael.pixelcrafted@gmail.com>
2025-03-24 00:34:00 +0100

    Update python_version.py

--- python_version.py
+++ python_version.py
@@ -1,23 +1,20 @@
 from time import time
 import numpy as np
-from numba import njit
+from numba import njit, prange
 
 
-@njit()
-def count_to_billion():
+@njit(parallel=True)
+def count_to_billion_parallel():
     random_array = np.empty(1000000000, dtype=np.int32)
 
-    for i in range(1000000000):
+    for i in prange(1000000000):
         random_array[i] = np.random.randint(1, 101)
-
-        if i % 100000000 == 0:
-            print(f"Counted to {i}")
 
     print("Created array of size:", len(random_array))
     return random_array
 
 
 start = time()
-result = count_to_billion()
+result = count_to_billion_parallel()
 end = time()
 print(f"Time taken: {end - start} seconds")
